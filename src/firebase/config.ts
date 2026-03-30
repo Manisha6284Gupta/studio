696c77f7717651d99a2992ff77c5c47ef036ddc5
Manisha Gupta <manishagupta01000@gmail.com>
2026-03-30 10:50:27 +0000

    firebase storage: No default bucket found. Did you set the storageBucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1065901514709:web:f207d2685991587e97675c",
   "apiKey": "AIzaSyCB4krGLZfiyr5rOmmZLndFdhrFUalqLHQ",
   "authDomain": "studio-4980755183-3866b.firebaseapp.com",
+  "storageBucket": "studio-4980755183-3866b.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1065901514709"
 };
